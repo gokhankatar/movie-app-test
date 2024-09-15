9588deff8e5ddd792e94ce2f2aa397de3d00f5ba
Gökhan Katar <katar_gokhan@hotmail.com>
2024-09-15 12:32:25 +0300

    started

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app theme="dark">
+    <v-main>
+      <v-container class="pa-5 pa-sm-8 pa-md-10 pa-lg-12 pa-xl-15">
+        <NuxtPage />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
