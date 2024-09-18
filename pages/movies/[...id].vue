20163f761350cf80ea8f9292712c720a20569217
gokhankatar <katar_gokhan@hotmail.com>
2024-09-18 11:31:09 +0300

    added single movie page

---
+++ pages/movies/[...id].vue
@@ -0,0 +1,125 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+const baseImageUrlLg = "https://image.tmdb.org/t/p/w1280";
+const baseImageUrlSm = "https://image.tmdb.org/t/p/w500";
+const baseImageUrlPosterLg = "https://image.tmdb.org/t/p/w500";
+const baseImageUrlPosterSm = "https://image.tmdb.org/t/p/w300";
+
+const movie = computed(() => {
+  const movieData = route.query.movie;
+  try {
+    return typeof movieData === "string" ? JSON.parse(movieData) : null;
+  } catch (error) {
+    console.error("Movie data parsing error:", error);
+    return null;
+  }
+});
+</script>
+
+<template>
+  <div v-if="movie">
+    <div
+      class="d-none d-sm-flex background-blur"
+      :style="{
+        backgroundImage: `url(${baseImageUrlLg + movie.backdrop_path})`,
+      }"
+    />
+    <div
+      class="d-flex d-sm-none background-blur"
+      :style="{
+        backgroundImage: `url(${baseImageUrlSm + movie.backdrop_path})`,
+      }"
+    />
+
+    <v-row
+      class="content-container fill-height w-100 d-flex justify-space-evenly align-center"
+    >
+      <v-col class="content" cols="12" sm="12" md="4">
+        <v-img
+          class="d-none d-sm-flex movie-poster rounded-lg"
+          :src="baseImageUrlPosterLg + movie.poster_path"
+          cover
+          height="700"
+        />
+        <v-img
+          class="d-flex d-sm-none movie-poster rounded-lg"
+          :src="baseImageUrlPosterSm + movie.poster_path"
+          cover
+          height="500"
+        />
+      </v-col>
+      <v-col
+        class="content content-text pa-5 rounded-lg d-flex flex-column ga-1"
+        cols="12"
+        sm="12"
+        md="6"
+      >
+        <h3 class="text-h3 text-white mb-7">{{ movie.original_title }}</h3>
+        <p class="text-subtitle-1 text-white">
+          <strong>Overview : </strong>{{ movie.overview }}
+        </p>
+        <p class="text-subtitle-1 text-white">
+          <strong>Release : </strong>{{ movie.release_date.slice(0, 4) }}
+        </p>
+        <p class="text-subtitle-1 text-white d-flex ga-2 justify-start align-center">
+          <strong>IMDB : </strong>{{ movie.vote_average.toFixed(1) }}
+          <v-icon color="#FB8D00" icon="mdi-star" />
+        </p>
+        <p class="text-subtitle-1 text-white d-flex ga-2 justify-start align-center">
+          <strong>Vote : </strong>{{ movie.vote_count }}
+          <v-icon color="#FB8D00" icon="mdi-thumbs-up-down" />
+        </p>
+        <p class="text-subtitle-1 text-white d-flex ga-2 justify-start align-center">
+          <strong>Views : </strong>{{ movie.popularity }}
+          <v-icon color="#FB8D00" icon="mdi-eye" />
+        </p>
+        <v-btn
+          @click="router.replace('/movies')"
+          variant="outlined"
+          class="back-btn my-3 rounded-lg"
+          size="large"
+          text="back to movies"
+        />
+      </v-col>
+    </v-row>
+  </div>
+  <div v-else>Loading movie details...</div>
+</template>
+
+<style scoped>
+.background-blur {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: blur(0.6rem);
+  z-index: 1;
+}
+.content-container {
+  position: relative;
+  z-index: 2;
+}
+.content {
+  z-index: 999;
+}
+.content-text {
+  background: rgba(0, 0, 0, 0.3);
+}
+.back-btn {
+  border: 1px solid #fff;
+  color: #fff;
+}
+.back-btn:hover {
+  border-color: red;
+  background-color: red;
+  color: #fff;
+}
+</style>
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,36 +1,11 @@
-<template>
-  <Loading v-if="store.isLoading" />
-
-  <v-row v-if="!store.isLoading" class="mt-5 d-flex flex-wrap align-center">
-    <v-col class="movie rounded-lg" v-for="item of store.data?.results" :key="item.id" cols="12" sm="6" md="4" lg="3">
-      <v-img height="350" cover class="movie-img transition rounded-lg" :src="baseImageUrl + item.backdrop_path" />
-      <span class="movie-rate pa-2 bg-deep-orange text-white">{{
-        item.vote_average.toFixed(1)
-      }}</span>
-      <v-img width="50" src="https://cdn-icons-png.flaticon.com/512/3686/3686899.png"
-        class="play-icon d-none cursor-pointer transition pa-5 rounded-xl" />
-      <div class="content pa-2 d-flex flex-column rounded-lg">
-        <p class="date text-white text-caption text-sm-subtitle-1">
-          {{ item.release_date.slice(0, 4) }}
-        </p>
-        <p class="movie-title text-white text-subtitle-1">
-          {{ item.original_title }}
-        </p>
-      </div>
-    </v-col>
-  </v-row>
-
-  <v-pagination v-if="!store.isLoading" v-model="store.currentPage" :length="store.data?.total_pages"
-    active-color="deep-orange" class="mt-15 transition" @input="onPageChange" />
-</template>
-
 <script lang="ts" setup>
 import { ref, onMounted, watch } from "vue";
 import { usePaginationStore } from "~/store/paginationStore.js";
 
+const router = useRouter();
 const isLoading = ref(false);
 const store = usePaginationStore();
-const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+const baseImageUrl = "https://image.tmdb.org/t/p/w300";
 
 onMounted(() => {
   isLoading.value = true;
@@ -48,7 +23,64 @@ watch(
     store.fetchData(newPage);
   }
 );
+const goToMovie = (item: any) => {
+  router.push({
+    path: `/movies/${item.id}`,
+    query: {
+      movie: JSON.stringify(item),
+    },
+  });
+};
 </script>
+
+<template>
+  <Loading v-if="store.isLoading" />
+
+  <v-row v-if="!store.isLoading" class="mt-5 d-flex flex-wrap align-center">
+    <v-col
+      class="movie rounded-lg"
+      v-for="item of store.data?.results"
+      :key="item.id"
+      cols="12"
+      sm="6"
+      md="4"
+      lg="3"
+    >
+      <v-img
+        height="580"
+        cover
+        class="movie-img transition rounded-lg"
+        :src="baseImageUrl + item.poster_path"
+      />
+      <span class="movie-rate pa-2 bg-deep-orange text-white">{{
+        item.vote_average.toFixed(1)
+      }}</span>
+      <v-img
+        @click="goToMovie(item)"
+        width="75"
+        src="https://cdn-icons-png.flaticon.com/512/3686/3686899.png"
+        class="play-icon d-none cursor-pointer transition pa-5 rounded-xl"
+      />
+      <div class="content pa-2 d-flex flex-column rounded-lg">
+        <p class="date text-white text-caption text-sm-subtitle-1">
+          {{ item.release_date.slice(0, 4) }}
+        </p>
+        <p class="movie-title text-white text-subtitle-1">
+          {{ item.original_title }}
+        </p>
+      </div>
+    </v-col>
+  </v-row>
+
+  <v-pagination
+    v-if="!store.isLoading"
+    v-model="store.currentPage"
+    :length="store.data?.total_pages"
+    active-color="deep-orange"
+    class="mt-15 transition"
+    @input="onPageChange"
+  />
+</template>
 
 <style scoped>
 @import url("/assets/css/main.css");
@@ -59,8 +91,8 @@ watch(
 
 .movie-rate {
   position: absolute;
-  top: 3%;
-  left: 4%;
+  top: 2%;
+  left: 2%;
   z-index: 2;
   border-radius: 0 0 1.1rem 0;
 }
@@ -71,6 +103,7 @@ watch(
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 999;
+  overflow: hidden;
 }
 
 .play-icon:hover {
