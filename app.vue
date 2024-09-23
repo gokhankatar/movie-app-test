56aa88b728f877731f312bfb719bae5be31f3505
gokhankatar <katar_gokhan@hotmail.com>
2024-09-23 19:17:50 +0300

    added seo ımg

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import store from "./store/store";
+import seoImg from "~/assets/img/seoImg.png";
 
 const _store = store();
 const isLoading = ref(true);
@@ -12,9 +13,9 @@ useSeoMeta({
     "Gkmovies is one of the GKSoftware web applications that contains movies. You can access more than 10,000 movies with the movie search engine using the TMDB API.",
   ogDescription:
     "Gkmovies is one of the GKSoftware web applications that contains movies. You can access more than 10,000 movies with the movie search engine using the TMDB API.",
-  ogImage: "/public/favicon.ico",
-  twitterCard: "/public/favicon.ico",
-  twitterImage: "/public/favicon.ico",
+  ogImage: seoImg,
+  twitterCard: seoImg,
+  twitterImage: seoImg,
 });
 
 onMounted(() => {
