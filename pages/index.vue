b0329a319e3e76f232547e386f7593a8e1f677ed
gokhankatar <katar_gokhan@hotmail.com>
2024-09-23 12:50:07 +0300

    added movies for genres

--- pages/index.vue
+++ pages/index.vue
@@ -40,8 +40,8 @@
       :modules="[SwiperFreeMode, SwiperAutoplay, SwiperNavigation]"
       :grab-cursor="true"
       :navigation="{
-        prevEl: '.custom-swiper-button-prev',
-        nextEl: '.custom-swiper-button-next',
+        prevEl: '.popular-swiper-button-prev',
+        nextEl: '.popular-swiper-button-next',
       }"
       :loop="true"
       :autoplay="{
@@ -62,12 +62,18 @@
           spaceBetween: 40,
         },
         '@1.50': {
-          slidesPerView: 4,
+          slidesPerView: 5,
           spaceBetween: 20,
         },
       }"
       class="mySwiper"
     >
+      <div class="popular-swiper-button-prev rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-left" size="x-large" />
+      </div>
+      <div class="popular-swiper-button-next rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-right" size="x-large" />
+      </div>
       <SwiperSlide class="swiper-slide" v-for="item of store.data?.results">
         <v-img
           class="rounded-xl d-none d-sm-flex"
@@ -81,9 +87,17 @@
           height="400"
           cover
         />
-        <div class="content d-flex flex-column ga-2 align-start pa-3">
-          <h3 class="text-subtitle-1 text-xl-h5">{{ item.original_title }}</h3>
-          <p class="text-subtitle-2 text-md-subtitle-1">{{ item.release_date }}</p>
+        <div class="content d-flex flex-column ga-1 align-start pa-3 rounded-lg">
+          <h3 class="text-subtitle-1 text-white text-xl-h5">{{ item.original_title }}</h3>
+          <p class="text-subtitle-2 text-white text-md-subtitle-1">
+            {{ item.release_date.slice(0, 4) }}
+          </p>
+          <div class="d-flex ga-1">
+            <span class="text-subtitle-1 text-white">{{
+              item.vote_average?.toFixed(1)
+            }}</span>
+            <v-icon icon="mdi-star" color="#FB8D00" />
+          </div>
         </div>
       </SwiperSlide>
     </swiper>
@@ -94,6 +108,304 @@
     <h3 class="text-h5 text-sm-h3 text-md-h2">Action</h3>
     <hr class="my-5 w-100 w-md-25" color="deep-orange" />
   </div>
+
+  <div v-if="!store.isLoading" class="mt-15">
+    <swiper
+      :spaceBetween="30"
+      :freeMode="true"
+      :modules="[SwiperFreeMode, SwiperAutoplay, SwiperNavigation]"
+      :grab-cursor="true"
+      :navigation="{
+        prevEl: '.action-swiper-button-prev',
+        nextEl: '.action-swiper-button-next',
+      }"
+      :loop="true"
+      :autoplay="{
+        delay: 4500,
+        disableOnInteraction: false,
+      }"
+      :breakpoints="{
+        '@0.00': {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        '@0.75': {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        '@1.00': {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        '@1.50': {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      }"
+      class="mySwiper"
+    >
+      <div class="action-swiper-button-prev rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-left" size="x-large" />
+      </div>
+      <div class="action-swiper-button-next rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-right" size="x-large" />
+      </div>
+      <SwiperSlide class="swiper-slide" v-for="item of store.actionMovies?.results">
+        <v-img
+          class="rounded-xl d-none d-sm-flex"
+          :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`"
+          height="450"
+          cover
+        />
+        <v-img
+          class="rounded-xl d-flex d-sm-none"
+          :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
+          height="400"
+          cover
+        />
+        <div class="content d-flex flex-column ga-1 align-start pa-3 rounded-lg">
+          <h3 class="text-subtitle-1 text-white text-xl-h5">{{ item.original_title }}</h3>
+          <p class="text-subtitle-2 text-white text-md-subtitle-1">
+            {{ item.release_date.slice(0, 4) }}
+          </p>
+          <div class="d-flex ga-1">
+            <span class="text-subtitle-1 text-white">{{
+              item.vote_average?.toFixed(1)
+            }}</span>
+            <v-icon icon="mdi-star" color="#FB8D00" />
+          </div>
+        </div>
+      </SwiperSlide>
+    </swiper>
+  </div>
+
+  <!-- thriller movies -->
+  <div v-if="!store.isLoading" class="mt-15">
+    <h3 class="text-h5 text-sm-h3 text-md-h2">Thriller</h3>
+    <hr class="my-5 w-100 w-md-25" color="deep-orange" />
+  </div>
+
+  <div v-if="!store.isLoading" class="mt-15">
+    <swiper
+      :spaceBetween="30"
+      :freeMode="true"
+      :modules="[SwiperFreeMode, SwiperAutoplay, SwiperNavigation]"
+      :grab-cursor="true"
+      :navigation="{
+        prevEl: '.thriller-swiper-button-prev',
+        nextEl: '.thriller-swiper-button-next',
+      }"
+      :loop="true"
+      :autoplay="{
+        delay: 4500,
+        disableOnInteraction: false,
+      }"
+      :breakpoints="{
+        '@0.00': {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        '@0.75': {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        '@1.00': {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        '@1.50': {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      }"
+      class="mySwiper"
+    >
+      <div class="thriller-swiper-button-prev rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-left" size="x-large" />
+      </div>
+      <div class="thriller-swiper-button-next rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-right" size="x-large" />
+      </div>
+      <SwiperSlide class="swiper-slide" v-for="item of store.thrillerMovies?.results">
+        <v-img
+          class="rounded-xl d-none d-sm-flex"
+          :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`"
+          height="450"
+          cover
+        />
+        <v-img
+          class="rounded-xl d-flex d-sm-none"
+          :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
+          height="400"
+          cover
+        />
+        <div class="content d-flex flex-column ga-1 align-start pa-3 rounded-lg">
+          <h3 class="text-subtitle-1 text-white text-xl-h5">{{ item.original_title }}</h3>
+          <p class="text-subtitle-2 text-white text-md-subtitle-1">
+            {{ item.release_date.slice(0, 4) }}
+          </p>
+          <div class="d-flex ga-1">
+            <span class="text-subtitle-1 text-white">{{
+              item.vote_average?.toFixed(1)
+            }}</span>
+            <v-icon icon="mdi-star" color="#FB8D00" />
+          </div>
+        </div>
+      </SwiperSlide>
+    </swiper>
+  </div>
+
+  <!-- horror movies -->
+  <div v-if="!store.isLoading" class="mt-15">
+    <h3 class="text-h5 text-sm-h3 text-md-h2">Horror</h3>
+    <hr class="my-5 w-100 w-md-25" color="deep-orange" />
+  </div>
+
+  <div v-if="!store.isLoading" class="mt-15">
+    <swiper
+      :spaceBetween="30"
+      :freeMode="true"
+      :modules="[SwiperFreeMode, SwiperAutoplay, SwiperNavigation]"
+      :grab-cursor="true"
+      :navigation="{
+        prevEl: '.horror-swiper-button-prev',
+        nextEl: '.horror-swiper-button-next',
+      }"
+      :loop="true"
+      :autoplay="{
+        delay: 4500,
+        disableOnInteraction: false,
+      }"
+      :breakpoints="{
+        '@0.00': {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        '@0.75': {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        '@1.00': {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        '@1.50': {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      }"
+      class="mySwiper"
+    >
+      <div class="horror-swiper-button-prev rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-left" size="x-large" />
+      </div>
+      <div class="horror-swiper-button-next rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-right" size="x-large" />
+      </div>
+      <SwiperSlide class="swiper-slide" v-for="item of store.horrorMovies?.results">
+        <v-img
+          class="rounded-xl d-none d-sm-flex"
+          :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`"
+          height="450"
+          cover
+        />
+        <v-img
+          class="rounded-xl d-flex d-sm-none"
+          :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
+          height="400"
+          cover
+        />
+        <div class="content d-flex flex-column ga-1 align-start pa-3 rounded-lg">
+          <h3 class="text-subtitle-1 text-white text-xl-h5">{{ item.original_title }}</h3>
+          <p class="text-subtitle-2 text-white text-md-subtitle-1">
+            {{ item.release_date.slice(0, 4) }}
+          </p>
+          <div class="d-flex ga-1">
+            <span class="text-subtitle-1 text-white">{{
+              item.vote_average?.toFixed(1)
+            }}</span>
+            <v-icon icon="mdi-star" color="#FB8D00" />
+          </div>
+        </div>
+      </SwiperSlide>
+    </swiper>
+  </div>
+
+  <!-- adventure movies -->
+  <div v-if="!store.isLoading" class="mt-15">
+    <h3 class="text-h5 text-sm-h3 text-md-h2">Adventure</h3>
+    <hr class="my-5 w-100 w-md-25" color="deep-orange" />
+  </div>
+
+  <div v-if="!store.isLoading" class="mt-15">
+    <swiper
+      :spaceBetween="30"
+      :freeMode="true"
+      :modules="[SwiperFreeMode, SwiperAutoplay, SwiperNavigation]"
+      :grab-cursor="true"
+      :navigation="{
+        prevEl: '.adventure-swiper-button-prev',
+        nextEl: '.adventure-swiper-button-next',
+      }"
+      :loop="true"
+      :autoplay="{
+        delay: 4500,
+        disableOnInteraction: false,
+      }"
+      :breakpoints="{
+        '@0.00': {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        '@0.75': {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        '@1.00': {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        '@1.50': {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      }"
+      class="mySwiper"
+    >
+      <div class="adventure-swiper-button-prev rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-left" size="x-large" />
+      </div>
+      <div class="adventure-swiper-button-next rounded-xl pa-5 cursor-pointer">
+        <v-icon icon="mdi-arrow-right" size="x-large" />
+      </div>
+      <SwiperSlide class="swiper-slide" v-for="item of store.adventureMovies?.results">
+        <v-img
+          class="rounded-xl d-none d-sm-flex"
+          :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`"
+          height="450"
+          cover
+        />
+        <v-img
+          class="rounded-xl d-flex d-sm-none"
+          :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
+          height="400"
+          cover
+        />
+        <div class="content d-flex flex-column ga-1 align-start pa-3 rounded-lg">
+          <h3 class="text-subtitle-1 text-white text-xl-h5">{{ item.original_title }}</h3>
+          <p class="text-subtitle-2 text-white text-md-subtitle-1">
+            {{ item.release_date.slice(0, 4) }}
+          </p>
+          <div class="d-flex ga-1">
+            <span class="text-subtitle-1 text-white">{{
+              item.vote_average?.toFixed(1)
+            }}</span>
+            <v-icon icon="mdi-star" color="#FB8D00" />
+          </div>
+        </div>
+      </SwiperSlide>
+    </swiper>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -103,10 +415,6 @@ import { usePaginationStore } from "~/store/paginationStore";
 const router = useRouter();
 const store = usePaginationStore();
 const randomPage = Math.floor(Math.random() * 100) + 1;
-
-// turune göre film getirmek
-// aksıyon gerne = 28
-// https://api.themoviedb.org/3/discover/movie?api_key=YOUR_API_KEY&with_genres=28&sort_by=popularity.desc&page=1
 
 onMounted(async () => {
   store.fetchData(randomPage);
@@ -167,14 +475,38 @@ onMounted(async () => {
   color: #fff !important;
 }
 
-.custom-swiper-button-prev,
-.custom-swiper-button-next {
+.popular-swiper-button-prev,
+.popular-swiper-button-next,
+.action-swiper-button-prev,
+.action-swiper-button-next,
+.horror-swiper-button-prev,
+.horror-swiper-button-next,
+.thriller-swiper-button-prev,
+.thriller-swiper-button-next,
+.adventure-swiper-button-prev,
+.adventure-swiper-button-next {
   position: absolute;
   top: 50%;
   z-index: 99;
-  background: rgba(0, 0, 0, 0.3);
+  transform: translateY(-50%);
+  background: rgba(0, 0, 0, 0.5);
   border-radius: 505;
   padding: 1rem;
   color: #ff5722;
+  z-index: 999;
+}
+.popular-swiper-button-prev,
+.action-swiper-button-prev,
+.horror-swiper-button-prev,
+.thriller-swiper-button-prev,
+.adventure-swiper-button-prev {
+  left: 1%;
+}
+.popular-swiper-button-next,
+.action-swiper-button-next,
+.horror-swiper-button-next,
+.thriller-swiper-button-next,
+.adventure-swiper-button-next {
+  right: 1%;
 }
 </style>
--- store/paginationStore.js
+++ store/paginationStore.js
@@ -4,17 +4,41 @@ export const usePaginationStore = defineStore('paginationStore', {
     state: () => ({
         currentPage: 1,
         data: null,
+        actionMovies: null,
+        thrillerMovies: null,
+        horrorMovies: null,
+        adventureMovies: null,
         isLoading: false,
     }),
     actions: {
         async fetchData(page) {
             this.isLoading = true;
             const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=4e012d6a950f5501f23ee3e7f1e548d4&language=en-US&page=${page}`;
+            const actionMoviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=4e012d6a950f5501f23ee3e7f1e548d4&with_genres=28";
+            const thrillerMoviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=4e012d6a950f5501f23ee3e7f1e548d4&with_genres=53"
+            const horrorMoviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=4e012d6a950f5501f23ee3e7f1e548d4&with_genres=27"
+            const adventureMoviesUrl = "https://api.themoviedb.org/3/discover/movie?api_key=4e012d6a950f5501f23ee3e7f1e548d4&with_genres=12"
 
             try {
                 const response = await fetch(url);
                 const result = await response.json();
                 this.data = result;
+
+                const responseAction = await fetch(actionMoviesUrl);
+                const resultAction = await responseAction.json();
+                this.actionMovies = resultAction;
+
+                const responseThriller = await fetch(thrillerMoviesUrl);
+                const resultThriller = await responseThriller.json();
+                this.thrillerMovies = resultThriller;
+
+                const responseHorror = await fetch(horrorMoviesUrl);
+                const resultHorror = await responseHorror.json();
+                this.horrorMovies = resultHorror;
+
+                const responseAdventure = await fetch(adventureMoviesUrl);
+                const resultAdventure = await responseAdventure.json();
+                this.adventureMovies = resultAdventure;
             } catch (error) {
                 console.error('API request failed:', error);
             } finally {
